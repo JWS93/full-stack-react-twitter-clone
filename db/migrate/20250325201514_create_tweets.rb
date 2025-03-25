2a8d04b1af9c770a7731a9996887866082e7a2fe
JWS93 <justinscott820@gmail.com>
2025-03-25 14:19:36 -0700

    table attributes added pre-migration

--- db/migrate/20250325201514_create_tweets.rb
+++ db/migrate/20250325201514_create_tweets.rb
@@ -1,7 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[6.1]
   def change
     create_table :tweets do |t|
-
+      t.string :message
+      t.integer :user_id
+      
       t.timestamps
     end
   end
