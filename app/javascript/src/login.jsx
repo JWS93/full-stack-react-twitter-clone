86d0f1a15fa46d0726b0bf46d1d7f15d178078a6
JWS93 <justinscott820@gmail.com>
2025-04-01 22:14:19 -0700

    some front end, login page

--- app/javascript/src/login.jsx
+++ app/javascript/src/login.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
+import './login.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const login = () => {
+
+
+export const Login = () => {
 	return (
-		
+		<React.Fragment>
+			<div className="container bg-image">
+				<div className="row">
+					<div className="col-10 my-5">
+						<div className="row">
+							<div className="col-lg-6 col-md-12 mt-5">
+								<FontAwesomeIcon icon="fa-brands fa-twitter" />
+								<p>Welcome to Twitter, the world's digital town square where you can connect and explore.</p>
+							</div>
+							<div className="col-lg-6 col-md-12" id="login">
+								<div className="Row border border-primary border-2 rounded login-box">
+									<div className="col-12 mx-2 mt-1">Username</div>
+									<input className="col-12 rounded mx-2" type="text" id="loginUsername" placeholder="username"></input>
+									<div className="col-12 mt-1 mx-2">Password</div>
+									<input className="col-12 rounded mx-2" type="text" id="loginPassword" placeholder="password"></input>
+									<button className="col-12 btn btn-sm btn-primary rounded my-2 mx-2">Log In</button>
+									<h5 className="col-12 mx-2 mt-4">New to Twitter?</h5>
+									<div className="col-12 mx-2 mt-1">Username</div>
+									<input className="col-12 rounded mx-2" type="text" id="signupUsername" placeholder="username"></input>
+									<div className="col-12 mx-2">Email</div>
+									<input className="col-12 rounded mx-2" type="text" id="sigupEmail" placeholder="email"></input>
+									<div className="col-12 mt-1 mx-2">Password</div>
+									<input className="col-12 rounded mx-2" type="text" id="signupPassword" placeholder="password"></input>
+									<button className="col-12 btn btn-sm btn-primary rounded my-2 mx-2">Log In</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
 	)
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = createRoot(document.body.appendChild(document.createElement('div')));
+  root.render(<Login />);
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-	root 'homepage#index'
-  get '/feeds' => 'feeds#index'
+	root to: 'pages#login'
 
-  # USERS
-  post '/users' => 'users#create'
+	namespace :api do
+		get '/feeds' => 'feeds#index'
+
+		# USERS
+		post '/users' => 'users#create'
 
 
-  # SESSIONS
-  post '/sessions' => 'sessions#create'
-  get '/authenticated' => 'sessions#authenticated'
-  delete '/sessions' => 'sessions#destroy'
-  # TWEETS
-  post '/tweets' => 'tweets#create'
-  delete '/tweets/:id' => 'tweets#destroy'
-  get '/tweets' => 'tweets#index'
-  get '/users/:username/tweets' => 'tweets#index_by_user'
+		# SESSIONS
+		post '/sessions' => 'sessions#create'
+		get '/authenticated' => 'sessions#authenticated'
+		delete '/sessions' => 'sessions#destroy'
+		# TWEETS
+		post '/tweets' => 'tweets#create'
+		delete '/tweets/:id' => 'tweets#destroy'
+		get '/tweets' => 'tweets#index'
+		get '/users/:username/tweets' => 'tweets#index_by_user'
 
-  # Redirect all other paths to index page, which will be taken over by AngularJS
-  get '*path' => 'homepage#index'
+		# Redirect all other paths to index page, which will be taken over by AngularJS
+		get '*path' => 'homepage#index'
+	end
 end
